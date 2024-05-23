f86b89271552399766b51679b4802c373db6cc38
Robert Wettstädt <robert@wettstaedt.dev>
2024-05-23 12:08:35 +0200

    build: upgrade drizzle-kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './src/lib/db/schema.ts',
   out: './drizzle',
-  driver: 'better-sqlite',
+  dialect: 'sqlite',
   dbCredentials: {
     url: './sqlite.db',
   },
