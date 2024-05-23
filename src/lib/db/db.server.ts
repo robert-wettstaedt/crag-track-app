9bbcf4ba435cd3ec29900907020fcf7bf876e01d
Robert Wettstädt <robert@wettstaedt.dev>
2024-05-23 12:32:56 +0200

    fix: pass complete db path

--- src/lib/db/db.server.ts
+++ src/lib/db/db.server.ts
@@ -1,8 +1,11 @@
 import { PUBLIC_DEMO_MODE } from '$env/static/public'
 import Database from 'better-sqlite3'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
+import path from 'path'
 import * as schema from './schema'
 
-const sqlite = new Database(`${PUBLIC_DEMO_MODE ? 'demo-db' : 'db'}/sqlite.db`)
+const dbPath = path.join(process.cwd(), PUBLIC_DEMO_MODE ? 'demo-db' : 'db', 'sqlite.db')
+
+const sqlite = new Database(dbPath)
 sqlite.exec('PRAGMA journal_mode = WAL;')
 export const db = drizzle(sqlite, { schema })
