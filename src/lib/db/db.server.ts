f193641e270910aeb997213d679ff6f5a59888fc
Robert Wettstädt <robert@wettstaedt.dev>
2024-05-23 12:43:15 +0200

    fix: use relative db path

--- src/lib/db/db.server.ts
+++ src/lib/db/db.server.ts
@@ -2,9 +2,12 @@ import { PUBLIC_DEMO_MODE } from '$env/static/public'
 import Database from 'better-sqlite3'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import * as schema from './schema'
 
-const dbPath = path.join(process.cwd(), PUBLIC_DEMO_MODE ? 'demo-db' : 'db', 'sqlite.db')
+const dirname = fileURLToPath(import.meta.url)
+const dbPath = path.join(dirname, '..', '..', '..', '..', PUBLIC_DEMO_MODE ? 'demo-db' : 'db', 'sqlite.db')
+console.log(dbPath)
 
 const sqlite = new Database(dbPath)
 sqlite.exec('PRAGMA journal_mode = WAL;')
