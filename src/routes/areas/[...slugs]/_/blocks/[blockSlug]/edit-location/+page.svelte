af204fb3c2b86bb937a725972e4cf07371ae9230
Robert Wettstädt <robert@wettstaedt.dev>
2024-06-14 20:36:25 +0200

    feat: allow manual input of coordinates

--- src/routes/areas/[...slugs]/_/blocks/[blockSlug]/edit-location/+page.svelte
+++ src/routes/areas/[...slugs]/_/blocks/[blockSlug]/edit-location/+page.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
   import { page } from '$app/stores'
-  import { AppBar } from '@skeletonlabs/skeleton'
+  import { AppBar, Tab, TabGroup } from '@skeletonlabs/skeleton'
   import type { Coordinate } from 'ol/coordinate'
+  import type { ChangeEventHandler } from 'svelte/elements'
 
   export let data
   export let form
   $: basePath = `/areas/${$page.params.slugs}/_/blocks/${$page.params.blockSlug}`
 
   let coordinate: Coordinate | null = null
+  let tabSet = 0
 
   const onChange = ({ detail }: CustomEvent<Coordinate>) => (coordinate = detail)
+
+  const getValue: ChangeEventHandler<HTMLInputElement> = (event): number => {
+    return Number((event.target as HTMLInputElement).value)
+  }
+
+  const onChangeLat: ChangeEventHandler<HTMLInputElement> = (event) => {
+    coordinate = [coordinate?.at(0), getValue(event)]
+  }
+
+  const onChangeLong: ChangeEventHandler<HTMLInputElement> = (event) => {
+    coordinate = [getValue(event), coordinate?.at(1)]
+  }
 </script>
 
 <AppBar>
@@ -30,12 +44,37 @@
   {/if}
 
   <div class="mt-8">
-    {#await import('$lib/components/BlocksMapWithAddableMarker/index.js') then BlocksMap}
-      <BlocksMap.default blocks={data.blocks} on:change={onChange} />
-    {/await}
+    <TabGroup>
+      <Tab bind:group={tabSet} name="tab1" value={0}>
+        <span>Map</span>
+      </Tab>
+      <Tab bind:group={tabSet} name="tab2" value={1}>
+        <span>Lat Long</span>
+      </Tab>
 
-    <input hidden name="lat" value={form?.lat ?? coordinate?.at(1)} />
-    <input hidden name="long" value={form?.long ?? coordinate?.at(0)} />
+      <svelte:fragment slot="panel">
+        {#if tabSet === 0}
+          {#await import('$lib/components/BlocksMapWithAddableMarker/index.js') then BlocksMap}
+            <BlocksMap.default blocks={data.blocks} on:change={onChange} />
+          {/await}
+
+          <input hidden name="lat" value={form?.lat ?? coordinate?.at(1)} />
+          <input hidden name="long" value={form?.long ?? coordinate?.at(0)} />
+        {:else if tabSet === 1}
+          <div class="flex flex-col gap-4">
+            <label class="label">
+              <span>Latitude</span>
+              <input class="input" name="lat" on:change={onChangeLat} value={form?.lat ?? coordinate?.at(1) ?? ''} />
+            </label>
+
+            <label class="label">
+              <span>Longitude</span>
+              <input class="input" name="long" on:change={onChangeLong} value={form?.long ?? coordinate?.at(0) ?? ''} />
+            </label>
+          </div>
+        {/if}
+      </svelte:fragment>
+    </TabGroup>
   </div>
 
   <div class="flex justify-between mt-8">
