1314c190c35442157986cc678a5ead60c7c670bf
Robert Wettstädt <robert@wettstaedt.dev>
2024-12-27 19:45:08 +0100

    chore: change rate limiting max value

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -17,7 +17,7 @@ export const config = {
   api: {
     rateLimit: {
       windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
+      max: 500, // limit each IP to 500 requests per windowMs
     },
   },
   cache: {
