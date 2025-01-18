e7275e891a2eb2a77ab7857310c3502e0d0e8637
Robert Wettstädt <robert@wettstaedt.dev>
2025-01-18 09:20:48 +0100

    fix: pseudo route to block’s routes

--- src/routes/(app)/areas/[...slugs]/_/blocks/[blockSlug]/routes/+page.server.ts
+++ src/routes/(app)/areas/[...slugs]/_/blocks/[blockSlug]/routes/+page.server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
 export const load = (({ params }) => {
-  redirect(301, `/areas/${params.slugs}/_/blocks/${params.blockSlug}#routes`)
+  redirect(301, `/areas/${params.slugs}/_/blocks/${params.blockSlug}#topo`)
 }) satisfies PageServerLoad
